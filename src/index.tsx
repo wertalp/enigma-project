0b6a07738201cb6ce8b5508d9fecfd5dc151d55b
Patrick Wertal <patrick.wertal@gmail.com>
2022-02-02 15:21:08 +0100

    init.test-aws

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
